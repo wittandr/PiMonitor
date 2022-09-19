f787b61d227ab43c94b576159436cc80d9fb72fc
Andreas Wittig <wittig_andreas@gmx.de>
2022-09-19 20:29:11 +0200

    Fullscreen and dark mode

--- server/mainapplication.py
+++ server/mainapplication.py
@@ -1,7 +1,10 @@
 from PyQt5 import uic, QtWidgets
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtCore import Qt
 from app import Server, DataSubject
 import threading
 import time
+import platform
 
 
 class MainApplication(QtWidgets.QMainWindow):
@@ -16,7 +19,26 @@ class MainApplication(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('FormFiles/main.ui', self)
-        self.show()
+        # Now use a palette to switch to dark colors:
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        palette.setColor(QPalette.WindowText, Qt.white)
+        palette.setColor(QPalette.Base, QColor(25, 25, 25))
+        palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        palette.setColor(QPalette.ToolTipBase, Qt.black)
+        palette.setColor(QPalette.ToolTipText, Qt.white)
+        palette.setColor(QPalette.Text, Qt.white)
+        palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        palette.setColor(QPalette.ButtonText, Qt.white)
+        palette.setColor(QPalette.BrightText, Qt.red)
+        palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        palette.setColor(QPalette.HighlightedText, Qt.black)
+        self.setPalette(palette)
+        if platform.machine() == "AMD64":
+            self.show()
+        else:
+            self.showFullScreen()
         self.data_subject = DataSubject()
         server = Server(self.data_subject)
         server_thread = threading.Thread(target=server.run)
